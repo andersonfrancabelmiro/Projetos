94fd3365e565f1e8563f12872fb4fdc67ea1de38
Anderson de França Belmiro <43147799+andersonfrancabelmiro@users.noreply.github.com>
2025-09-04 20:44:35 -0300

    Add pasta de Laços de Repetição

---
+++ Laços de Repetição/Contar de 1 a 10.py
@@ -0,0 +1,5 @@
+contador = int(input("Digite um numero: "))
+i = 1
+while i <= contador :
+    print(i)
+    i += 1
---
+++ Laços de Repetição/Percorrer uma lista de frutas.py
@@ -0,0 +1,11 @@
+contador = (input("Digite um fruta: "))
+i = 1
+while i <= 4 :
+    print(i)
+    i += 1
+futras =  []
+
+
+#print(futras[i])
+
+
